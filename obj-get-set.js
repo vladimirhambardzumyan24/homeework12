8d850800521db3729a60497a0f7f5ab2dc21f160
Vladimir Hambardzumyan <vladimirhambardzumyan24@gmail.com>
2021-05-17 14:17:20 +0400

    Object with getter/setterfield name.

---
+++ obj-get-set.js
@@ -0,0 +1,15 @@
+const obj = {
+  _name: [], // ['name', length][]
+  set name(value) {
+    let item = value.split(", ");
+    for (let i = 0; i < item.length; i++) {
+      obj._name.push([item[i], item[i].length]);
+    }
+    var count = item.length;
+  },
+  get name() {
+    return obj._name;
+  },
+};
+obj.name = "Vrezh, Artavazd";
+console.log(obj.name);
